b20be48d78e4b11b973630fb2710d952b4c88654
wfilho91 <wellington79620@gmail.com>
2021-03-29 15:58:45 -0300

    Created Project

---
+++ backend/src/main/java/com/devsuperior/finalworkcap01/FinalWorkCap01Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.finalworkcap01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalWorkCap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalWorkCap01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/finalworkcap01/FinalWorkCap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.finalworkcap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalWorkCap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
